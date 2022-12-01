9bd1aacbbb76333a99d9693cfeef92df1a37b916
Will <rovria@my.yorku.ca>
2022-12-01 17:45:51 -0500

    v1.1

--- locomotive.js
+++ locomotive.js
@@ -1,13 +1,12 @@
 const locoScroll = new LocomotiveScroll({
-    el: document.querySelector(".locomotive-scroll"),
+  el: document.querySelector('.locomotive-scroll'),
+  smooth: true,
+  smartphone: {
     smooth: true,
-    smartphone: {
-        smooth: true
-    },
-    tablet: {
-        smooth: true
-    },
-      smoothMobile: 1,
-    multiplier: 1.0,
-  }); 
-  
+  },
+  tablet: {
+    smooth: true,
+  },
+  smoothMobile: 1,
+  multiplier: 1.0,
+})
