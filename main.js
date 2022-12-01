90fabd43ce33ea32e97404484d2ecd72124cec4c
Will <rovria@my.yorku.ca>
2022-12-01 18:13:24 -0500

    v1.1.3

--- main.js
+++ main.js
@@ -175,3 +175,181 @@ $(document).ready(function () {
     },
   )
 })
+
+//Project Card THREE - Animation 
+var textWrapper = document.querySelector('.projectthree_title')
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>",
+)
+
+$(document).ready(function () {
+  $('.card_imgthree').hover(
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.projectthree_title .letter',
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i,
+      })
+    },
+
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.projectthree_title .letter',
+        translateX: [0, -30],
+        opacity: [1, 0],
+        easing: 'easeInExpo',
+        duration: 1100,
+        delay: (el, i) => 100 + 30 * i,
+      })
+    },
+  )
+})
+//Project Card One - Category Label Animation
+var textWrapper = document.querySelector('.project_three_label')
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>",
+)
+
+$(document).ready(function () {
+  $('.card_imgthree').hover(
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.project_three_label .letter',
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 800,
+        delay: (el, i) => 150 * (i + 1),
+      })
+    },
+
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.project_three_label .letter',
+        opacity: 0,
+        duration: 800,
+        easing: 'easeOutExpo',
+        delay: 1000,
+      })
+    },
+  )
+})
+//Project Card One - Project Star Animation [Universal] 
+var textWrapper = document.querySelector('.project_three_card_star')
+$(document).ready(function () {
+  $('.card_imgthree').hover(
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.project_three_card_star',
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 800,
+        delay: (el, i) => 150 * (i + 1),
+      })
+    },
+
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.project_three_card_star',
+        opacity: 0,
+        duration: 800,
+        easing: 'easeOutExpo',
+        delay: 1000,
+      })
+    },
+  )
+})
+
+//Project Card FOUR - Animation 
+var textWrapper = document.querySelector('.projectfour_title')
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>",
+)
+
+$(document).ready(function () {
+  $('.card_imgfour').hover(
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.projectfour_title .letter',
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i,
+      })
+    },
+
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.projectfour_title .letter',
+        translateX: [0, -30],
+        opacity: [1, 0],
+        easing: 'easeInExpo',
+        duration: 1100,
+        delay: (el, i) => 100 + 30 * i,
+      })
+    },
+  )
+})
+//Project Card One - Category Label Animation
+var textWrapper = document.querySelector('.project_four_label')
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>",
+)
+
+$(document).ready(function () {
+  $('.card_imgfour').hover(
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.project_four_label .letter',
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 800,
+        delay: (el, i) => 150 * (i + 1),
+      })
+    },
+
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.project_four_label .letter',
+        opacity: 0,
+        duration: 800,
+        easing: 'easeOutExpo',
+        delay: 1000,
+      })
+    },
+  )
+})
+//Project Card One - Project Star Animation [Universal] 
+var textWrapper = document.querySelector('.project_four_card_star')
+$(document).ready(function () {
+  $('.card_imgfour').hover(
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.project_four_card_star',
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 800,
+        delay: (el, i) => 150 * (i + 1),
+      })
+    },
+
+    function () {
+      anime.timeline({ loop: false }).add({
+        targets: '.project_four_card_star',
+        opacity: 0,
+        duration: 800,
+        easing: 'easeOutExpo',
+        delay: 1000,
+      })
+    },
+  )
+})
