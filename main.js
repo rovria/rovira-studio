5672054938598fbba04993846279f7afe4caf0fb
William <rovria@my.yorku.ca>
2022-12-29 23:02:59 -0500

    v3.1

--- main.js
+++ main.js
@@ -531,3 +531,38 @@ $(document).ready(function () {
     }
   );
 });
+
+$(function () {
+  $('.project_card').slice(0, 4).show();
+  $('#loadMore').on('click', function (e) {
+    e.preventDefault();
+    $('.project_card:hidden').slice(0, 4).slideDown();
+    if ($('.project_card:hidden').length == 0) {
+      $('#load').fadeOut('slow');
+    }
+    $('html,body').animate(
+      {
+        scrollTop: $(this).offset().top,
+      },
+      1500
+    );
+  });
+});
+
+$('a[href=#top]').click(function () {
+  $('body,html').animate(
+    {
+      scrollTop: 0,
+    },
+    600
+  );
+  return false;
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+    $('.totop a').fadeIn();
+  } else {
+    $('.totop a').fadeOut();
+  }
+});
