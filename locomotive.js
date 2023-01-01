ce816b435ef0169d8cb0b4270b89345f38942ecb
William <rovria@my.yorku.ca>
2023-01-01 18:34:42 -0500

    v3.1

--- locomotive.js
+++ locomotive.js
@@ -4,13 +4,14 @@ const locoObserver = new ResizeObserver(() => scroll.update()).observe(
 
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector('.locomotive-scroll'),
+  lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
   smooth: true,
   smartphone: {
-    smooth: true,
+    smooth: false,
   },
   tablet: {
     smooth: true,
   },
-  smoothMobile: 1,
+  smoothMobile: 0,
   multiplier: 1.0,
 });
