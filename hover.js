ef6d19272d0e19998705303063437b78930d5aac
William <rovria@my.yorku.ca>
2023-01-03 03:18:07 -0500

    v3.1

--- hover.js
+++ hover.js
@@ -6,7 +6,7 @@ var cardOne = new hoverEffect({
   image2:
     'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/63ac08e5eb51b0c5732033d5_project_sylvia_2.webp',
   displacementImage:
-    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.webp',
+    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.png',
 });
 
 var cardTwo = new hoverEffect({
@@ -17,7 +17,7 @@ var cardTwo = new hoverEffect({
   image2:
     'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/63ac08a2eb96a7bfc7a799a8_Mask%20group%20(2)%20(1).webp',
   displacementImage:
-    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.webp',
+    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.png',
 });
 
 var cardThree = new hoverEffect({
@@ -28,7 +28,7 @@ var cardThree = new hoverEffect({
   image2:
     'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/63ac03437a18d4784fafb0ec_project2%20(2).webp',
   displacementImage:
-    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.webp',
+    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.png',
 });
 
 var cardFour = new hoverEffect({
@@ -39,7 +39,7 @@ var cardFour = new hoverEffect({
   image2:
     'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/63ac088ad827c588c9423b81_project_frisky_2.webp',
   displacementImage:
-    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.webp',
+    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.png',
 });
 
 var cardFive = new hoverEffect({
@@ -50,7 +50,7 @@ var cardFive = new hoverEffect({
   image2:
     'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/63ae5de333698b76d1e77d68_project_exopro_2.webp',
   displacementImage:
-    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.webp',
+    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.png',
 });
 
 var cardSix = new hoverEffect({
@@ -61,5 +61,5 @@ var cardSix = new hoverEffect({
   image2:
     'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/63ae5dd7f706a12920e14ead_project_weedsimple_2.webp',
   displacementImage:
-    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.webp',
+    'https://uploads-ssl.webflow.com/634e33e838bd6483829835be/636b5e1c8c4c67634588db11_4.png',
 });
